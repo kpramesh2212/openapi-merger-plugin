b9fd2b4e91a6751b05997036c1794b537d9bfe7c
Narmada M <narmada.murugan1@gmail.com>
2023-02-06 17:19:37 +0000

    Fix: Add support for sub folders https://github.com/kpramesh2212/openapi-merger-plugin/issues/4

--- openapi-merger-app/src/main/kotlin/com/rameshkp/openapi/merger/app/OpenApiMergerApp.kt
+++ openapi-merger-app/src/main/kotlin/com/rameshkp/openapi/merger/app/OpenApiMergerApp.kt
@@ -36,6 +36,7 @@ class OpenApiMergerApp {
             it 
         }.forEach {
             log.debug("Parsing OpenAPI file {}", it.absolutePath)
+            parseOptions.isResolve = true
             val openAPI = OpenAPIV3Parser().read(it.absolutePath, null, parseOptions)
             openApiMerger.merge(openAPI)
         }
